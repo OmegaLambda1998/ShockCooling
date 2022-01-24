316f3fff9f9d09259107cea0d6137e05348145b8
Patrick Armstrong <u6096820@anu.edu.au>
2022-01-24 18:48:21 +1100

    Got models (maybe) working

--- src/Models.jl
+++ src/Models.jl
@@ -7,13 +7,14 @@ using Unitful, UnitfulAstro
 using Distributions
 
 # Exports
+export get_model
 export run_model
 export get_constraints
 
 abstract type Model end
 
 # Load in all models
-# Models should export themselves
+# All models must export themselves
 model_path = joinpath(@__DIR__, "models")
 for path in readdir(model_path)
     if isfile(joinpath(model_path, path))
@@ -21,25 +22,29 @@ for path in readdir(model_path)
     end
 end
 
+function get_model(model_name::String)
+    return getfield(Models, Symbol(model_name))
+end
+
 function run_model(model::Model, param::Dict, observation::Observation)
     error("generic run_model is being used, no method specified for model $(typeof(model))")
 end
 
-function get_constraints(constraint_dict::Dict)
+function get_constraints(constraints_dict::Dict)
     constraints = Dict()
     for param in keys(constraints_dict)
         param_dict = constraints_dict[param]
         param_unit = uparse(param_dict["unit"])
         param_prior = getfield(Distributions, Symbol(param_dict["prior"]))
-        param_values = param_dict["values"] .* param_unit
-        param_min = get(param_dict, "min", -Inf) .* param_unit
-        param_max = get(param_dict, "max", Inf) .* param_unit
+        param_values = param_dict["values"]
+        param_min = get(param_dict, "min", -Inf)
+        param_max = get(param_dict, "max", Inf)
         if param_prior == Normal
             prior = TruncatedNormal(param_values..., param_min, param_max)
         else
             prior = TruncatedNormal(param_prior(param_values...), param_min, param_max)
         end
-        constraints[param] = prior
+        constraints[param] = (prior, param_unit)
     end
     return constraints
 end
--- src/ShockCooling.jl
+++ src/ShockCooling.jl
@@ -1,8 +1,8 @@
 module ShockCooling
 
 # External packages
-using ArgParse
 using Supernovae
+using LoggingExtras
 using TOML
 
 # Internal Packages
@@ -10,62 +10,141 @@ include("Models.jl")
 using .Models
 
 # Exports
-export run_mcmc
+export run_shockcooling
 
-# Data function
-function load_data(data_config::Dict)
-    input_path = joinpath(dirname(@__DIR__), "Inputs")
-    data_path = data_config["data_path"]
-    if !isabspath(data_path)
-        data_path = joinpath(input_path, data_path)
+function setup_global_config!(toml::Dict)
+    config = get(toml, "global", Dict())
+    # Base path is where everything relative will be relative to
+    # Defaults to the directory containing the toml path
+    # Can be relative (to the toml path) or absolute
+    base_path = get(config, "base_path", nothing)
+    if isnothing(base_path)
+        base_path = dirname(toml["toml_path"])
+    elseif !isabspath(base_path)
+        base_path = joinpath(dirname(toml["toml_path"]), base_path)
     end
-    return Supernova(data_path)
+    base_path = abspath(base_path)
+    config["base_path"] = base_path
+    # Output path is where all output (figures) will be placed
+    # Defaults to base_path / Output
+    # Can be relative (to base_path) or absolute
+    output_path = get(config, "output_path", nothing)
+    if isnothing(output_path)
+        output_path = joinpath(base_path, "Output")
+    elseif !isabspath(output_path)
+        output_path = joinpath(base_path, output_path)
+    end
+    config["output_path"] = abspath(output_path)
+    # Data path is where all supernovae data (both photometric and spectroscopic) will be stored
+    # Default to base_path / Data
+    # Can be relatvie (to base_path) or absolute
+    data_path = get(config, "data_path", nothing)
+    if isnothing(data_path)
+        data_path = joinpath(base_path, "Data")
+    elseif !isabspath(data_path)
+        data_path = joinpath(base_path, data_path)
+    end
+    config["data_path"] = abspath(data_path)
+    # Logging sets whether or not to setup and use Supernovae's logging
+    logging = get(config, "logging", false)
+    config["logging"] = logging
+    # Log file is the name of the log file. This will only work if logging is true
+    # Can only be relative to output_path
+    # Defaults to log.txt
+    log_file = get(config, "log_file", nothing)
+    if logging
+        if isnothing(log_file)
+            log_file = "log.txt"
+        end
+        log_file = abspath(joinpath(output_path, log_file))
+    end
+    if !logging & !isnothing(log_file)
+        @warn "Logging set to false, so log file $log_file will not be written. Please add `logger=true` to your [ global ] config"
+    end
+    config["log_file"] = log_file
+    toml["global"] = config
 end
 
-# Model function
-function load_model(model_config::Dict)
-    constraint_dict = model_config["constraints"]
-    constraints = get_constraints(constraint_dict)
-    model = getfield(Models, Symbol(model_config["name"]))
+function setup_logger(log_file::AbstractString, verbose::Bool)
+    if verbose
+        level = Logging.Debug
+    else
+        level = Logging.Info
+    end
+    function fmt(io, args)
+        if args.level == Logging.Error
+            color = :red
+            bold = true
+        elseif args.level == Logging.Warn
+            color = :yellow
+            bold = true
+        elseif args.level == Logging.Info
+            color = :cyan
+            bold = false
+        else
+            color = :white
+            bold = false
+        end
+        printstyled(io, args._module, " | ", "[", args.level, "] ", args.message, "\n"; color = color, bold = bold)
+    end
+    logger = TeeLogger(
+        MinLevelLogger(FormatLogger(fmt, open(log_file, "w")), level),
+        MinLevelLogger(FormatLogger(fmt, stdout), level)
+    )
+    global_logger(logger)
+    @info "Logging to $log_file"
+end
+
+function setup_model(model_dict::Dict)
+    # TODO Add the ability to change parameter names
+    model = get_model(model_dict["name"])
+    constraints = get_constraints(model_dict["constraints"])
     return model(constraints)
 end
 
-function load_model(model_list::Vector)
-    return load_model.(model_list)
-end
-
-# Main function
-function run_mcmc(toml::Dict)
-    supernova = load_data(toml["data"])
-    @show supernova.name
-    models = load_model(toml["model"])
-    @show models
-end
-
-function run_mcmc(toml_path::AbstractString)
-    toml = TOML.parsefile(toml_path)
-    return run_mcmc(toml)
-end
-
-function get_args()
-    s = ArgParseSettings()
-    @add_arg_table s begin
-        "--verbose", "-v"
-            help = "Increase level of logging verbosity"
-            action = :store_true
-        "toml"
-            help = "Path to toml input file"
-            required = true
+function run_shockcooling(toml::Dict, verbose::Bool)
+    setup_global_config!(toml)
+    config = toml["global"]
+    # Ensure all path's exist
+    # TODO test
+    if !isdir(config["base_path"])
+        mkpath(config["base_path"])
     end
-
-    return parse_args(s)
+    if !isdir(config["output_path"])
+        mkpath(config["output_path"])
+    end
+    # Optionally set up logging
+    if config["logging"]
+        setup_logger(config["log_file"], verbose)
+    end
+    # Get data
+    # Data can either be stored in the "data_path" toml file
+    # Or can be specified via [ data.global ] [ data.observations ] etc...
+    # TODO test
+    @info "Loading in data"
+    data_path = get(toml["data"], "data_path", nothing)
+    if isnothing(data_path)
+        @debug "Using data in original toml"
+        # Assuming all relevant info is in the [ data ] block
+        supernova = process_supernova(toml["data"], verbose)
+    else
+        if !isabspath(data_path)
+            data_path = joinpath(config["base_path"], data_path)
+        end
+        @debug "Using data in $data_path"
+        supernova = process_supernova(data_path, verbose)
+    end
+    # Get model
+    # You must specify the model name as it appears in src/models (i.e P15)
+    @info "Loading in $(length(toml["model"])) models"
+    models = [setup_model(model_dict) for model_dict in toml["model"]]
+    @debug "Loaded in $(length(models)) models"
 end
 
-if abspath(PROGRAM_FILE) == @__FILE__
-    args = get_args()
-    verbose = args["verbose"]
-    toml_path = args["toml"]
-    run_mcmc(toml_path)
+function run_shockcooling(toml_path::AbstractString, verbose::Bool)
+    toml = TOML.parsefile(toml_path)
+    toml["toml_path"] = abspath(toml_path)
+    return run_shockcooling(toml, verbose)
 end
 
 end # module
